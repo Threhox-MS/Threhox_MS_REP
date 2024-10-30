7ffa0affe57c35db676dfa43b7b6e6a27e232c5f
Threhox-MS <schoenebergmax@gmail.com>
2024-10-30 16:08:10 +0100

    Add files via upload

---
+++ 7_CastConvertCase.sql
@@ -0,0 +1,36 @@
+
+-- ------------------------------------------------------------------
+-- 7.1: Aufgabe 3.7 soll nun mit dem genauen Durchschnittswert 
+-- (mit dezimalstellen) berechnet werden
+-- ------------------------------------------------------------------
+
+
+-- ------------------------------------------------------------------
+-- 7.2: Selektiere alle Trainer von Schulungen, die durchgeführten 
+-- Schulungen sowie einmal die Durchschnittsnote, die beste und die 
+-- schlechteste Note je Schulung.
+-- ------------------------------------------------------------------
+
+
+-- ------------------------------------------------------------------
+-- 7.3: Die Abfrage aus Aufgabe 7.2 soll nun nicht mehr alle Noten 
+-- einzelnd darstellen, sondern eine Spalte in der alle drei selektierten
+-- Noten nebeneinander stehen. Beispiel: 3.3551
+-- ------------------------------------------------------------------
+
+
+-- ------------------------------------------------------------------
+-- 7.4: Selektiere das aktuelle Datum in unterschiedlichen Formaten:
+-- Standard, US, Deutschland (jeweils mit und ohne Jahrhundert) 
+-- ------------------------------------------------------------------
+
+
+
+-- ------------------------------------------------------------------
+-- 7.5: Führe die folgenden Casts durch:
+-- Zeitstempel -> Date
+-- Den eigenen vollen Namen -> varchar(5)
+-- Das eigene Geburtsdatum -> datetime
+-- Die eigene Körpergröße in m -> int
+-- Was fällt auf?
+-- ------------------------------------------------------------------
